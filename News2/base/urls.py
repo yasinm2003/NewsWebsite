d9c6b85ee3c75aa6d1093f5ebb8f1f4be44b1272
Yasin Mohammadi <mohammadiyasin2003@gmail.com>
2023-09-12 15:00:16 +0330

    updated login page and logout user

--- News2/base/urls.py
+++ News2/base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.HomePage, name="home"),
     path('login/', views.LoginPage, name="login"),
+    path('logout/', views.LogoutUage, name="logout"),
     path('register/', views.RegisterPage, name="register"),
     path('favorite/<str:pk>/', views.FavoritePage, name="favorite"),
     path('createnews/', views.createnewsPage, name="createnews"),
--- News2/base/views.py
+++ News2/base/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic, News, Comment
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 from django.db.models import Q
 
 def HomePage(request):
@@ -14,8 +17,27 @@ def HomePage(request):
 
 def LoginPage(request):
     page = 'login'
-    context = {}
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':    
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            message = messages.error(request, "This username dose not exist")
+        user = authenticate(request, username=username, password=password)
+        if user != None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "username or password dose not match")
+    context = {'page':page}
     return render(request, 'base/loginregister.html', context)
+
+def LogoutUage(request):
+    logout(request)
+    return redirect('home')
 
 def RegisterPage(request):
     page = 'register'
